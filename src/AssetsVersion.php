80bf090e341578f941f55b2f7ae98f5897a60146
Raman Verma <devramanverma@gmail.com>
2023-10-09 17:17:04 +0530

    Added: AssetsVersion.php

---
+++ src/AssetsVersion.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace TheCoderRaman\AssetsVersion;
+
+use Illuminate\Contracts\Config\Repository;
+use Illuminate\Contracts\Foundation\Application;
+use TheCoderRaman\AssetsVersion\Contracts\AssetsVersionInterface;
+
+class AssetsVersion implements AssetsVersionInterface
+{
+    /**
+     * The application instance.
+     *
+     * @var Application
+     */
+    protected Application $app;
+
+    /**
+     * The config instance.
+     *
+     * @var Repository
+     */
+    protected Repository $config;
+
+    /**
+     * All versioned files
+     *
+     * @var array
+     */
+    protected array $versioned = [];
+
+    /**
+     * Construct hashids manager instance
+     *
+     * @param Application $app
+     * @param Repository $config
+     * @return void
+     */
+    public function __construct(
+        Application $app, Repository $config
+    ) {
+        $this->app = $app;
+        $this->config = $config;
+
+        $this->refreshVersionLookup();
+    }
+
+    /**
+     * Get config name
+     *
+     * @return string
+     */
+    public function getConfigName(): string
+    {
+        return 'assets-version';
+    }
+
+    /**
+     * Get the asset path.
+     *
+     * @return string
+     */
+    public function getAssetPath()
+    {
+        $assetPath = parse_url(
+            asset('/'), PHP_URL_PATH
+        );
+
+        return realpath(
+            $this->app->basePath($assetPath)
+        );
+    }
+
+    /**
+     * Get the asset css path.
+     *
+     * @param string $path
+     * @param bool $fullUrl
+     * @return string
+     */
+    public function cssPath(
+        string $path, bool $fullUrl = false
+    ): string {
+        return $this->assetPath(
+            "css/" . ltrim($path, "/"), $fullUrl
+        );
+    }
+
+    /**
+     * Get the asset js path.
+     *
+     * @param string $path
+     * @param bool $fullUrl
+     * @return string
+     */
+    public function jsPath(
+        string $path, bool $fullUrl = false
+    ): string {
+        return $this->assetPath(
+            "js/" . ltrim($path, "/"), $fullUrl
+        );
+    }
+
+    /**
+     * Get the asset path.
+     *
+     * @param string $path
+     * @param bool $fullUrl
+     * @return string
+     */
+    public function assetPath(
+        string $path, bool $fullUrl = false
+    ): string {
+        $path = ltrim($path, "/");
+
+        if (empty($path)) {
+            return asset('/');
+        }
+
+        if (!isset(
+            $this->versioned[$path]
+        )) {
+            return asset($path);
+        }
+
+        $pathVersion = $this->versioned[$path];
+
+        if(!$fullUrl){
+            return asset("{$path}?v={$pathVersion}");
+        }
+
+        return url(asset("{$path}?v={$pathVersion}"));
+    }
+
+    /**
+     * Refresh version lookup array.
+     *
+     * @return void
+     */
+    public function refreshVersionLookup(): void
+    {
+        $versionFilePath = (
+            $this->getVersionFilePath()
+        );
+
+        if (!file_exists($versionFilePath)) {
+            $this->versioned = [];
+            return;
+        };
+
+        $versioned = require $versionFilePath;
+
+        $this->versioned = array_merge(
+            $versioned['versions'],
+            $this->getNamedConfig("versioned", [])
+        );
+    }
+
+    /**
+     * Get assets versioning file path.
+     *
+     * @return string
+     */
+    public function getVersionFilePath(): string
+    {
+        return $this->app->storagePath(
+            "cache/assets-version/versioned.php"
+        );
+    }
+
+    /**
+     * Get the given named configuration.
+     *
+     * @param string $name
+     * @param mixed $default
+     * @return string|null
+     */
+    public function getNamedConfig(
+        string $name, $default = null
+    ) {
+        return $this->config->get(
+            $this->getConfigName() . '.' . $name, $default
+        );
+    }
+}
